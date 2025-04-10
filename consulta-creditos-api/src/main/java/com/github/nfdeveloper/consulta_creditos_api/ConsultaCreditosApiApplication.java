108a3f65eb28db27cba435f38a70538529fe6903
nfdeveloper <nfdeveloper04@gmail.com>
2025-04-10 16:49:11 -0300

    feat: criacao projeto backend e instalacao de dependencias

---
+++ consulta-creditos-api/src/main/java/com/github/nfdeveloper/consulta_creditos_api/ConsultaCreditosApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.nfdeveloper.consulta_creditos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaCreditosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaCreditosApiApplication.class, args);
+	}
+
+}
---
+++ consulta-creditos-api/src/test/java/com/github/nfdeveloper/consulta_creditos_api/ConsultaCreditosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nfdeveloper.consulta_creditos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaCreditosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

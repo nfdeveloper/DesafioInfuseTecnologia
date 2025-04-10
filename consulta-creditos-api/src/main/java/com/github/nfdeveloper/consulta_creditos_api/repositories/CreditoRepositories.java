e35bcbca0d1f16aee68d336c5917d9d89fc46471
nfdeveloper <nfdeveloper04@gmail.com>
2025-04-10 17:12:13 -0300

    fix: renomeando classe do repositorio.

--- consulta-creditos-api/src/main/java/com/github/nfdeveloper/consulta_creditos_api/repositories/CreditoRepositories.java
+++
@@ -1,9 +0,0 @@
-package com.github.nfdeveloper.consulta_creditos_api.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.github.nfdeveloper.consulta_creditos_api.models.entitites.Credito;
-
-public interface CreditoRepositories extends JpaRepository<Credito, Long>{
-
-}

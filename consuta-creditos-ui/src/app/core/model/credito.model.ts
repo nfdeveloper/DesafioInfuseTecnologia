78287541a9179fc405e680542f642196cbbc1d8a
nfdeveloper <nfdeveloper04@gmail.com>
2025-04-10 23:51:35 -0300

    feat: criacao do projeto front end e criacao da entidade credito.

---
+++ consuta-creditos-ui/src/app/core/model/credito.model.ts
@@ -0,0 +1,13 @@
+export class Credito {
+    id?: number;
+    numeroCredito?: string;
+    numeroNfse?: string;
+    dataConstituicao?: Date;
+    valorIssqn?: number;
+    tipoCredito?: string;
+    simplesNacional?: boolean;
+    aliquota?: number;
+    valorFaturado?: number;
+    valorDeducao?: number;
+    baseCalculo?: number;
+}
